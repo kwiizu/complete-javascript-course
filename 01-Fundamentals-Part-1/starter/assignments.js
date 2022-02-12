f17ca5070021999db08c4b6f58a95c66ef95cc14
kwiizu <itsalexwu@gmail.com>
2022-02-12 02:08:51 +0100

    created script.js
    section1 part 14.

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,19 @@
+
+/* const country = 'Sweden';
+const continent = 'Europe';
+const population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+const language = 'Swedish';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'finnish'; */
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,72 @@
+/* let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName)
+console.log(firstName)
+
+//Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+
+let person = 'Jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob); 
+*/
+
+/* let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Alex');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); */
+
+/* let age = 30;
+age = 31;
+
+const birthYear = 1987;
+//birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher';
+
+let lastName = 'Wu';
+console.log(lastName); */
+
+const now = 2037
+const ageAlex = now - 1987;
+const ageSarah = now - 1989;
+console.log(ageAlex, ageSarah);
+
+console.log(ageAlex * 2, ageAlex / 10, 2 ** 3);
+// 2 ** 3 means 2 to power of 3 = 2 * 2 * 2
+
+const firstName = 'Alex';
+const lastName = 'Wu';
