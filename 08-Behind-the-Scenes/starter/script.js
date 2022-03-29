5a0f0c7d627aa3550a478aafce838f8e50d8e00e
kwiizu <itsalexwu@gmail.com>
2022-03-29 09:49:30 +0200

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,18 +1,80 @@
 'use strict';
 
-const name = 'Jonas';
+// function calcAge(birthYear) {
+//   const age = 2022 - birthYear;
 
-const first = () => {
-  let a = 1;
-  const b = second(7, 9);
-  a = a + b;
-  console.log(a);
-  return a;
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = 'Steven';
+//       output = 'new output';
+//       const str = `you are're a millenial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial);
+//   }
+//   printAge();
+
+//   return age;
+// }
+
+// const firstName = 'Alex';
+// calcAge(1987);
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+//   greet: () => console.log(`hey ${this}`),
+// };
+// jonas.greet();
+
+// const calcAge = birthYear => birthYear + 2;
+
+// console.log(calcAge(1));
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
 };
 
-function second(x, y) {
-  var c = 2;
-  return c;
-}
-const x = first();
-console.log(x);
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+// console.log(jessica);
+// console.log(marriedJessica);
+
+// copying object
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log(jessica2);
+console.log(jessicaCopy);
+
+cl;
